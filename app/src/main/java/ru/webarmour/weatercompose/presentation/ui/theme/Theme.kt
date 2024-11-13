c07c0f556b78ce0ce806232027b6b84a02f56c9d
Rrr <romanchekmezov99@gmail.com>
2024-11-13 13:10:18 +0300

    added favourite screen

--- app/src/main/java/ru/webarmour/weatercompose/presentation/ui/theme/Theme.kt
+++ app/src/main/java/ru/webarmour/weatercompose/presentation/ui/theme/Theme.kt
@@ -34,7 +34,7 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun WeaterComposeTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = false,
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
